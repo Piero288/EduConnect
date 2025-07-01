0481337205e898fa3e0b787d7a08fb3afacc5f2f
Piero Galatà <pgalata@xeniaprogetti.it>
2025-07-01 16:22:48 +0200

    updated auth-service main.py file

--- auth-service/main.py
+++ auth-service/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from controller.auth_controller import auth_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(auth_blueprint)
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9050, debug=True)
