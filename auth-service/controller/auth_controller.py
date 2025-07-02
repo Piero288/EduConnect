5e28dbf3074cbed91cab1890675c592e3f6663fe
Piero Galatà <pgalata@xeniaprogetti.it>
2025-07-02 12:40:21 +0200

    updated code of auth-service and added log

--- auth-service/controller/auth_controller.py
+++ auth-service/controller/auth_controller.py
@@ -1,28 +1,43 @@
 from flask import Blueprint, jsonify, request
 from service.auth_service import generate_token, verify_token
+from configuration.config import logger
 
 auth_blueprint = Blueprint('auth', __name__)
 
 @auth_blueprint.route('/authenticate', methods=['POST'])
 def authenticate():
+    logger.info(f"A new authenticate request has arrived.")
+    
     data = request.json
     user_id = data.get('user_id')
+    user_email = data.get('user_email')
+    
+    if not user_id or not user_email:
+        logger.error("Missing userId or email")
+        return jsonify({"success": False, "error": "Missing userId or email"}), 400
 
-    if not user_id:
-        return jsonify({"success": False, "error": "User ID missing"}), 400
-
-    token = generate_token(user_id)
+    token = generate_token(user_id, user_email)
+    logger.info(f"Token generated successfully: {token}")
     return jsonify({"success": True, "token": token}), 200
+
 
 @auth_blueprint.route('/verify_token', methods=['POST'])
 def verify_token_route():
+    logger.info("A new token verification request has arrived.")
+    
     token = request.json.get('token')
     if not token:
+        logger.error("Token missing")
         return jsonify({"success": False, "error": "Token missing"}), 400
 
     valid, payload_or_error = verify_token(token)
     if valid:
-        return jsonify({"valid": True, "user_id": payload_or_error})
+        logger.info("Token verified successfully")
+        return jsonify({"valid": True,
+                        "user_id": payload_or_error["user_id"],
+                        "user_email": payload_or_error["user_email"]
+                        }), 200
     else:
+        logger.error(f"{payload_or_error}")
         return jsonify({"valid": False, "error": payload_or_error}), 401
 
--- auth-service/service/auth_service.py
+++ auth-service/service/auth_service.py
@@ -1,10 +1,16 @@
 import jwt
-from configuration.config import SECRET_KEY
+import os
+from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
 
-def generate_token(user_id, expiration_minutes=60):
+load_dotenv()
+
+SECRET_KEY = os.getenv('JWT_SECRET', 'your_secret_key')
+
+def generate_token(user_id, user_email, expiration_minutes=60):
     payload = {
         'user_id': user_id,
+        'user_email': user_email,
         'exp': datetime.now(timezone.utc) + timedelta(minutes=expiration_minutes)
     }
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
@@ -14,7 +20,11 @@ def verify_token(token):
     try:
         decoded_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
         user_id = decoded_token.get('user_id')
-        return True, user_id
+        user_email= decoded_token.get("user_email")
+        return True, {
+            "user_id": user_id,
+            "user_email": user_email
+        }
     except jwt.ExpiredSignatureError:
         return False, "Token expired"
     except jwt.InvalidTokenError:
