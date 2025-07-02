a055a025af194d2aa73bea28475a73c0907b5146
Piero Galatà <pgalata@xeniaprogetti.it>
2025-07-02 12:39:49 +0200

    updated ode of api-gateway

--- api-gateway/main.py
+++ api-gateway/main.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, jsonify
-import requests, os
-from configuration.config import SERVICE_MAP
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SERVICE_MAP = {
+    "auth": os.getenv("AUTH_SERVICE_URL"),
+    "users": os.getenv("USER_SERVICE_URL"),
+    "courses": os.getenv("COURSE_SERVICE_URL"),
+}
 
 app = Flask(__name__)
 
